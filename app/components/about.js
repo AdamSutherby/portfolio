a5eea743f398b4f7d363bb2b09465dc74d80d911
AdamSutherby <123500035+AdamSutherby@users.noreply.github.com>
2024-04-28 01:16:30 -0600

    my god

--- app/components/about.js
+++ app/components/about.js
@@ -4,7 +4,7 @@ export default function About() {
             <h1 className="text-4xl font-mono text-center mt-40">I am Adam Sutherby,</h1>
         <div className="flex flex-row mx-auto max-w-[60%] mt-10 justify-center">
             <p className="w-25vw w-1/2 font-mono text-lg pr-1 pt-8 text-slate-400">
-            a passionate software and web application developer dedicated to creating impactful digital solutions. With a strong foundation in coding and a creative problem-solving approach, I specialize in building intuitive and efficient applications tailored to meet user needs. Driven by my love for innovation and continuous learning, I thrive in collaborative environments where I can contribute fresh ideas and work closely with teams to deliver exceptional results. I'm excited about the opportunity to apply my skills and contribute to meaningful projects. Let's connect and explore how we can work together to make a difference through technology! Currently looking for new opportunities.
+            a passionate software and web application developer dedicated to creating impactful digital solutions. With a strong foundation in coding and a creative problem-solving approach, I specialize in building intuitive and efficient applications tailored to meet user needs. Driven by my love for innovation and continuous learning, I thrive in collaborative environments where I can contribute fresh ideas and work closely with teams to deliver exceptional results. I am excited about the opportunity to apply my skills and contribute to meaningful projects. Let us connect and explore how we can work together to make a difference through technology! Currently looking for new opportunities.
             </p>
             <img
                 src="/images/adam-sutherby.png"
