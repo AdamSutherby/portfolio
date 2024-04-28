f48f49f760fdec6b1e47e05709bdbb42fa7a4a5a
AdamSutherby <123500035+AdamSutherby@users.noreply.github.com>
2024-04-28 00:31:49 -0600

    quick troubleshooting?

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Intro from './components/intro'
-import NavBar from './components/navBar.js'
+import Header from './components/header.js'
 import Proficiency from './components/proficiency.js'
 import About from './components/about.js'
 import Projects from './components/projects.js'
@@ -7,7 +7,7 @@ import Projects from './components/projects.js'
 export default function Page() {
   return (
     <div className='bg-slate-950 h-screen w-screen relative'>
-      <NavBar />
+      <Header />
       <div className="h-12 w-full absolute top-0 z-10 bg-gradient-to-b from-slate-950 to-transparent"></div>
       <div className="overflow-auto max-h-screen">
         <Intro />
